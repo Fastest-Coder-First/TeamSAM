85a6f3a27e801fb2cc5f023fbbf4edf84102022a
Saakshi <79035589+Si1verKnight@users.noreply.github.com>
2023-06-24 11:58:26 +0530

    Updated weather.py
    Added display function

--- weather.py
+++ weather.py
@@ -1,15 +1,13 @@
-# Function that calls openweather API to forecast weather o city gives as input
-
 import requests
 import json
 import os
 import sys
-from PIL import Image
 
+from PIL import Image
 from io import BytesIO
 
-def weather(city):
-
+# Function that calls openweather API
+def get_weather(city):
     # API Key and URL
     api_key = 'ce5672977fa34059f0b8992a98123ca2'
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city, api_key)
@@ -19,7 +17,6 @@ def weather(city):
 
     # Convert response to json
     data = response.json()
-
     
     # Check if the city exists
     if data["cod"] == 404:
@@ -28,7 +25,11 @@ def weather(city):
     # Handle web error
     if data["cod"] != 200:
         return "Error hitting the API."
-    
+
+        # Get country and city name
+    country = data["sys"]["country"]
+    city_name = data["name"]
+
     # Get weather description
     weather_description = data["weather"][0]["description"]
 
@@ -37,27 +38,15 @@ def weather(city):
     temp = temp - 273.15
     temp = round(temp, 2)
 
-    print(data)
-
     # Get humidity
     humidity = data["main"]["humidity"]
 
     # Get wind speed
     wind_speed = data["wind"]["speed"]
 
-    # Get country
-    country = data["sys"]["country"]
-    city_name = data["name"]
-
     # Get sunrise and sunset
     sunrise = data["sys"]["sunrise"]
-    sunset = data["sys"]["sunset"]
-
-    # Get timezone
-    timezone = data["timezone"]
-
-    # Get coordinates
-    coordinates = data["coord"]
+    sunset = data["sys"]["sunset"]    
 
     # Get weather icon
     weather_icon = data["weather"][0]["icon"]
@@ -69,26 +58,45 @@ def weather(city):
     weather_icon_description = data["weather"][0]["main"]
 
     # Return weather data
-    return { "weather_description": weather_description, "temp": temp, "humidity": humidity, "wind_speed": wind_speed, "country": country, "sunrise": 
-            sunrise, "sunset": sunset, "timezone": timezone, "city_name": city_name, "coordinates": coordinates, "weather_icon_url": 
-            weather_icon_url, "weather_icon_description": weather_icon_description }
-
+    return {"Country": country, "City": city_name, 
+            "Weather Description": weather_description, 
+            "Temperature": temp, "Humidity": humidity, "Wind Speed": wind_speed, 
+            "Sunrise": sunrise, "Sunset": sunset, 
+            "Weather Icon URL": weather_icon_url, "Weather Icon Description": weather_icon_description}
 
 
 # To view the image icon
-def imaginy(city):
-    weather_data = weather(city)
-    weather_icon_url = weather_data["weather_icon_url"]
-    print(weather_data)
-    response = requests.get(weather_icon_url)
+def display_icon(icon_url):
+    response = requests.get(icon_url)
     img = Image.open(BytesIO(response.content))
     img.show()
 
 
-if len(sys.argv) < 2:
+# To display the weather data
+def display_weather(city):
+    weather_data = get_weather(city)
+
+    print("City: {}, {}".format(weather_data["Country"], weather_data["City"]))
+    print("Weather Description: {}".format(weather_data["Weather Description"]))
+    print("Temperature: {}".format(weather_data["Temperature"]))
+    print("Humidity: {}".format(weather_data["Humidity"]))
+    print("Wind Speed: {}".format(weather_data["Wind Speed"]))
+    print("Sunrise: {}".format(weather_data["Sunrise"]))
+    print("Sunset: {}".format(weather_data["Sunset"]))
+    display_icon(weather_data["Weather Icon URL"])
+    print("Weather Icon Description: {}".format(weather_data["Weather Icon Description"]))
+
+
+
+# Test
+if __name__ == "__main__":
+    display_weather("London")
+
+
+'''if len(sys.argv) < 2:
     print("Usage: python weather.py <city_name>")
     sys.exit(1)
 
 # Get the city name from the command line argument
 city_name = sys.argv[1]
-imaginy(city_name)
+imaginy(city_name)'''
