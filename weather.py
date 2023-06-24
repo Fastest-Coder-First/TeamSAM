494f1171827a3304ac796cff14d589c2a88d7048
Saakshi <79035589+Si1verKnight@users.noreply.github.com>
2023-06-24 17:31:29 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,12 +5,20 @@ import click
 from io import BytesIO
 from colorama import Fore
 
+'''
+This program uses the OpenWeather API to get weather data for a given city.
+The user can either enter the city name or use their current location.
+The program uses the ipify API to get the user's IP address and then uses the ipapi API to get the location data.
+The click library is used to get the user's input. It also provides a help option for the user.
+The colorama library is used to add colors to the output.
+'''
 
 # Function that calls openweather API
+# Returns weather data for given city
 def get_weather(city):
 
     # API Key and URL
-    api_key = 'ce5672977fa34059f0b8992a98123ca2'
+    api_key = '' # Insert your API Key here
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city, api_key)
         
     # API call
@@ -22,11 +30,11 @@ def get_weather(city):
 
     # Check if API call was successful
     if st_code != 200:      
-        if st_code == 404:
+        if st_code == 404: # If city not found
             print(Fore.RED + "City not found." + Fore.RESET)
-        elif st_code == 401:
+        elif st_code == 401: # If API key is invalid
             print(Fore.RED + "Missing/Invalid API key." + Fore.RESET)
-        else:
+        else: 
             print(Fore.RED + "Something went wrong." + Fore.RESET)
         return None
 
@@ -57,7 +65,7 @@ def get_weather(city):
 def display_weather(city):
     try:
         weather_data = get_weather(city)
-        if weather_data is None:
+        if weather_data is None: # If API call was unsuccessful
             return
 
         print(Fore.GREEN+"City: {}, {}".format(weather_data["Country"],weather_data["City"]) + Fore.RESET)
@@ -72,13 +80,14 @@ def display_weather(city):
         print(Fore.RED + "Error getting weather data for given city." + Fore.RESET)
     
 
-# Function to get IP address
+# Function to get IP address using ipify API to obtain current location of user
 def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
     return response["ip"]
 
 
 # Function to get location
+# Returns location data
 def get_location():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
@@ -93,12 +102,15 @@ def get_location():
 
 # Main
 @click.command()
-@click.option("--city", metavar="CITY_NAME", help="Specify the city for which you want to get weather information.")
+@click.option("--city",  metavar="CITY_NAME", help="Specify the city for which you want to get weather information.")
 @click.option("--current-location", is_flag=True, help="Use your current location.")
 def main(city, current_location):
+
+    # Check if city name is given
     if current_location:
         location = get_location()
         city = location["city"]
+         
     elif not city:
         print(Fore.RED + "Missing required --city argument or --current-location flag." + Fore.RESET)
         print(Fore.YELLOW + "Enter: \n1 - To use your current location \n2 - To enter city name" + Fore.RESET)
